3ae4d42905565b6e03f7839e91cd81d61f3ae8e0
Hyrkan <hyrkangamez@gmail.com>
2025-02-10 08:58:51 +0800

    test

--- server/src/routes/GuestRoutes.ts
+++ server/src/routes/GuestRoutes.ts
@@ -1,9 +1,16 @@
 // src/routes/inquiryRoutes.ts
 import express from 'express';
+import { Request, Response, Router } from 'express';
+
 import { createInquiry } from '../controllers/Guest';
 
 const router = express.Router();
 
 router.post('/inquiries', createInquiry);
+
+router.get('/welcome', (req: Request, res: Response): void => {
+    res.send('Hi, welcome guest!');
+});
+
 
 export default router;
