3ec3e10bca2feaacd9f21d283aa0867eb894e16d
JMT <tjuanmiguel116@gmail.com>
2025-03-24 15:47:51 +0800

    adding some hearts

--- client/src/Guest/Landingpage/Components/Slide5.tsx
+++ client/src/Guest/Landingpage/Components/Slide5.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import bedCaro from "../../../assets/images/bedcaro.png"
 import seasonsofLove from "../../../assets/images/love.png"
 import bellIcon from "../../../assets/icons/bell_icon.png"
+import heartLeft from "../../../assets/icons/heart_left.png"
+import heartRight from "../../../assets/icons/heart_right.png"
 
 const Slide5: React.FC = () => {
   return (
@@ -25,19 +27,31 @@ const Slide5: React.FC = () => {
                 </p>
             </div>
 
-            {/*RIGHT*/}
-            <div className="absolute top-[50%] top-1/2 right-[50px] transform -translate-y-1/2 w-[550px] 
-            h-[450px] flex flex-col justify-center items-center">
-            <img className="w-full h-full" src={seasonsofLove} alt="Seasons of Love"/>
-            <button className="ml-[1%] absolute top-[70%] left-[150px] px-4 py-2 rounded-[15px] transition
-                bg-[#E5C81E] hover:bg-[#C4A817] hover:scale-105 w-[280px] h-[60px] flex items-center gap-[5px] 
-                border-2 border-black mt-[3%]">
-                <img className="ml-[2%] w-[13%] h-[60%]" src={bellIcon} />
-                <span className="text-[30px] font-bold text-white">RESERVATION</span>
-            </button>
+            {/* RIGHT */}
+            <div className="absolute top-1/2 right-[50px] transform -translate-y-1/2 w-[550px] h-[450px] flex flex-col justify-center items-center">
+                {/* Image */}
+                <img className="w-full h-full" src={seasonsofLove} alt="Seasons of Love" />
+                
+                {/* Hearts Decoration (Now on top of the button) */}
+                <div className="absolute top-[67%] left-1/2 transform -translate-x-1/2 flex gap-[20px] z-10 pt-[3%]">
+                    <img className="w-[30px] h-[30px]" src={heartLeft} alt="Heart Left" />
+                    <img className="w-[25px] h-[25px]" src={heartLeft} alt="Heart Left" />
+                    <img className="w-[25px] h-[25px]" src={heartRight} alt="Heart Right" />
+                    <img className="w-[30px] h-[30px]" src={heartRight} alt="Heart Right" />
+                </div>
 
-            <span className="ml-[5%] mt-[-10%] text-[20px]">Special promo for 2pax</span>
+                {/* Button */}
+                <button className="absolute top-[70%] left-[150px] px-4 py-2 rounded-[15px] transition
+                    bg-[#E5C81E] hover:bg-[#C4A817] hover:scale-105 w-[280px] h-[60px] flex items-center gap-[20px] 
+                    border-2 border-black mt-[3%]">
+                    <img className="ml-[2=%] w-[13%] h-[60%]" src={bellIcon} alt="Bell Icon" />
+                    <span className="text-[25px] font-bold text-white">RESERVE NOW</span>
+                </button>
+
+                {/* Special Promo Text */}
+                <span className="ml-[5%] mt-[-10%] text-[20px]">Special promo for 2pax</span>
             </div>
+
             
       </div>
   );
